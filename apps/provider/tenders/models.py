2cc0e021c2f9e3ca82d076370691a687a20d2ed2
iseldmuca77 <mucaiseld@gmail.com>
2025-05-01 23:01:58 +0200

    feat: tenders

--- apps/provider/tenders/models.py
+++ apps/provider/tenders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.TextChoices):
+    DRAFT = "Draft", "Draft"
+    PUBLISHED = "Published", "Published"
+    OPEN = "Open", "Open"
+    UNDER_EVALUATION = "Under Evaluation", "Under Evaluation"
+    CLOSED = "Closed", "Closed"
+    AWARDED = "Awarded", "Awarded"
+
+class TradesInstruments(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    open_date = models.DateTimeField()
+    expire_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=Status, default='Draft')
+    max_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    category = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.title
